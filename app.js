1f7e0edc77c3b5b04d2c2eac60d313b383abb639
tomer7 <tomer66@icloud.com>
2022-01-15 01:51:45 +0200

    update work+home

--- app.js
+++ app.js
@@ -4,75 +4,144 @@ const date = require(__dirname + "/date.js")
 
 
 const app = express();
-const items = ["Buy Food","Cook Food","Eat Food"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
 const hiddenItems = [];
-const workItems = []
+const workItems = ["Add Your Work To Do"]
+const homeItems = ["Add Your Home To Do"]
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.use(express.static("public"));
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   const day = date.getDate();
-  res.render('list', {listTitle: day, newListItems: items});
+  res.render('list', {
+    listTitle: day,
+    newListItems: items
+  });
 
 })
 
-app.post("/", function(req,res){
-  if (req.body.trashClick !== undefined)
-  {
-    for (var i = 0; i < items.length; i++) {
-      if (req.body.trashClick === items[i])
-      {
-        var theRemoved = items.splice(i, 1);
-        break;
-      }
-    }
-    res.redirect("/");
-    console.log("trashClick")
-  }
-  if (req.body.hideClick !== undefined)
-  {
-    for (var i = 0; i < items.length; i++) {
-      if (req.body.hideClick === items[i])
-      {
-        hiddenItems.push(items[i]);
-        var theRemoved = items.splice(i, 1);
-        break;
-      }
-    }
-    res.redirect("/");
-    console.log("hideClick")
-  }
-  if (req.body.newItem !== undefined)
-  {
-    const item = req.body.newItem;
+app.post("/", function(req, res) {
 
-    if (req.body.list === "Work") {
+  if (req.body.list === "Work" || req.body.whichList === "Work") { // inside work list
+    if (req.body.trashClick !== undefined) {
+      for (var i = 0; i < workItems.length; i++) {
+        if (req.body.trashClick === workItems[i]) {
+          workItems.splice(i, 1);
+          break;
+        }
+      }
+      res.redirect("/work");
+      console.log("trashClick")
+    }
+    if (req.body.hideClick !== undefined) {
+      for (var i = 0; i < workItems.length; i++) {
+        if (req.body.hideClick === workItems[i]) {
+          hiddenItems.push(workItems[i]);
+          workItems.splice(i, 1);
+          break;
+        }
+      }
+      res.redirect("/work");
+      console.log("hideClick")
+    }
+    if (req.body.newItem !== undefined) {
+      const item = req.body.newItem;
+
       workItems.push(item);
       res.redirect("/work")
+
     }
-    else
-    {
+  } else if (req.body.list === "Home" || req.body.whichList === "Home") { // inside home list
+    if (req.body.trashClick !== undefined) {
+      for (var i = 0; i < homeItems.length; i++) {
+        if (req.body.trashClick === homeItems[i]) {
+          homeItems.splice(i, 1);
+          break;
+        }
+      }
+      res.redirect("/home");
+      console.log("trashClick")
+    }
+    if (req.body.hideClick !== undefined) {
+      for (var i = 0; i < homeItems.length; i++) {
+        if (req.body.hideClick === homeItems[i]) {
+          hiddenItems.push(homeItems[i]);
+          homeItems.splice(i, 1);
+          break;
+        }
+      }
+      res.redirect("/home");
+      console.log("hideClick")
+    }
+    if (req.body.newItem !== undefined) {
+      const item = req.body.newItem;
+
+      homeItems.push(item);
+      res.redirect("/home")
+    }
+  } else { // inside regular list
+    if (req.body.trashClick !== undefined) {
+      for (var i = 0; i < items.length; i++) {
+        if (req.body.trashClick === items[i]) {
+          items.splice(i, 1);
+          break;
+        }
+      }
+      res.redirect("/");
+      console.log("trashClick")
+    }
+    if (req.body.hideClick !== undefined) {
+      for (var i = 0; i < items.length; i++) {
+        if (req.body.hideClick === items[i]) {
+          hiddenItems.push(items[i]);
+          items.splice(i, 1);
+          break;
+        }
+      }
+      res.redirect("/");
+      console.log("hideClick")
+    }
+    if (req.body.newItem !== undefined) {
+      const item = req.body.newItem;
+
       items.push(item)
       res.redirect("/");
     }
-  }
+
+  } // else closer - regular list
+
+
 })
 
-app.get("/work", function(req,res) {
-  res.render("list", {listTitle: "Work List", newListItems: workItems})
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  })
 })
 
-app.post("/work", function(req,res) {
-  const item = req.body.newItem;
-  workItems.push(item);
-  res.redirect("/work")
-})
 
-app.get("/about", function(req,res) {
+app.get("/about", function(req, res) {
   res.render("about")
 })
 
-app.listen(3000, function(){
+app.get("/home", function(req, res) {
+  res.render("list", {
+    listTitle: "Home List",
+    newListItems: homeItems
+  })
+})
+
+app.get("/hidden", function(req, res) {
+  res.render("list", {
+    listTitle: "Your Hidden List",
+    newListItems: hiddenItems
+  })
+})
+
+app.listen(3000, function() {
   console.log("Server on 3000")
 })
