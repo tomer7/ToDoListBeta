4156dfffbd2459a6a0f41185b0c733d1adbe688a
tomer7 <tomer66@icloud.com>
2022-02-10 13:35:38 +0200

    Add gitignore, Procfile and update ports

--- app.js
+++ app.js
@@ -284,8 +284,12 @@ app.post("/returnFromHide", function(req,res){
 })
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 
-app.listen(3000, function() {
-  console.log("Server on 3000")
+app.listen(port, function() {
+  console.log("Server has started Successfully")
 })
